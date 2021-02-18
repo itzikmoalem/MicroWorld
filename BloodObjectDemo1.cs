7927cd5b28dcd5c7c2f66cb0782ed0951b8b5545
itzikmoalem <65330570+itzikmoalem@users.noreply.github.com>
2021-02-18 14:14:03 +0200

    Add files via upload

---
+++ BloodObjectDemo1.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class BloodObjectDemo1
+{
+    public GameObject obj;
+    public int amount;
+    public int minStage;
+    public string scriptName;
+}
